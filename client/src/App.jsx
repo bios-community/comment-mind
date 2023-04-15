e58a9268b043d186a68143d768ee6d083580b8fc
inclinedadarsh <dubeyadarshmain@gmail.com>
2023-04-16 01:15:21 +0530

    Added basic files for Routing and to start coding

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,21 @@
+import { Routes, Route } from "react-router-dom";
+import Nav from "./components/Nav/Nav";
+import { useState } from "react";
+
 function App() {
-	return <div className="App">Hello World</div>;
+	const [link, setLink] = useState("");
+	return (
+		<>
+			<Nav />
+			<Routes>
+				<Route path="/" element={<Home setLink={setLink} />} />
+				<Route
+					path="/comment-analysis"
+					element={<CommentAnalysis link={link} />}
+				/>
+			</Routes>
+		</>
+	);
 }
 
 export default App;
