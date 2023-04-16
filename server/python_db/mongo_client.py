eee4548fb0ba431b1b92a302c3dda00b66284739
Pranav <pranavprajapati586@gmail.com>
2023-04-16 23:38:14 +0530

    Stored classified comments in different lists

--- server/python_db/mongo_client.py
+++ server/python_db/mongo_client.py
@@ -5,8 +5,16 @@ from youtube_api import YoutubeAPI
 
 # The function returns 4 parameters
 comments, video_title, subscriber_count, channel_name=YoutubeAPI(api_key=api_key,video_url="https://youtu.be/UpsZDGutpZc")
-print(video_title,subscriber_count,channel_name)
 
 comment_classifier = pkl.load(open("Pickle_file/question_classifier.pkl",'rb'))
 vectorizer = pkl.load(open("Pickle_file/text_vectorizer.pkl",'rb'))
+
+interrogative_comments = []
+feedbacks = []
+for comment in comments:
+    label = comment_classifier.predict(vectorizer.transform([comment]))
+    if label in ["whQuestion","yAnswer","nAnswer","ynQuestion"]:
+        interrogative_comments.append(comment)
+    else:
+        feedbacks.append(comment)
 
