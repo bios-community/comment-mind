f1653ebcc208f11a2e43bddf55cf362d621265f8
inclinedadarsh <dubeyadarshmain@gmail.com>
2023-04-16 11:27:32 +0530

    feat: added Nav component

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -1,8 +1,22 @@
 import React from "react";
+import Logo from "../../assets/logo.svg";
+import GithubMark from "../../assets/github-mark.svg";
+
 import "./Nav.scss";
 
 const Nav = () => {
-	return <div>Nav</div>;
+	return (
+		<nav className="nav container">
+			<img src={Logo} alt="logo" className="nav__logo-mark" />
+			<a
+				href="https://github.com/bios-community/comment-mind"
+				className="nav__link"
+			>
+				<img src={GithubMark} alt="GitHub Logo" />
+				GitHub
+			</a>
+		</nav>
+	);
 };
 
 export default Nav;
