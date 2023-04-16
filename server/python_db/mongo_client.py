d9169c37aeca53dc03427bcdcb35e5d3326ae77c
Pranav <pranavprajapati586@gmail.com>
2023-04-17 01:02:41 +0530

    Sucessfully updated databse with two new collections

--- server/python_db/mongo_client.py
+++ server/python_db/mongo_client.py
@@ -10,15 +10,26 @@ comment_classifier = pkl.load(open("Pickle_file/question_classifier.pkl",'rb'))
 vectorizer = pkl.load(open("Pickle_file/text_vectorizer.pkl",'rb'))
 
 interrogative_comments = []
-feedbacks = []
+feedback_comments = []
 for comment in comments:
     label = comment_classifier.predict(vectorizer.transform([comment]))
     if label in ["whQuestion","yAnswer","nAnswer","ynQuestion"]:
         interrogative_comments.append(comment)
     else:
-        feedbacks.append(comment)
+        feedback_comments.append(comment)
 
 # Start saving comments to database in Different collections
 
 atlas_client = MongoClient(AtlasConnection_string)
 
+#Create a Database
+comments_db = atlas_client.comments_db
+#Create Two collections
+questions = comments_db.questions
+feedbacks = comments_db.feedbacks
+
+questions.insert_many([{'question':question} for question in interrogative_comments])
+print("Question inserted in Database")
+
+feedbacks.insert_many([{'feedback':feedback} for feedback in feedback_comments])
+print("Feedbacks also inserted")
