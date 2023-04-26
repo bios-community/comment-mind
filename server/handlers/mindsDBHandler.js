226a9aded0914af63f3ca942a99377f3e91599d3
inclinedadarsh <dubeyadarshmain@gmail.com>
2023-04-26 19:33:07 +0530

    tried doing things with mindsDBHandler still a long way to go...

--- server/handlers/mindsDBHandler.js
+++ server/handlers/mindsDBHandler.js
@@ -11,5 +11,54 @@ await MindsDB.default.connect({
 	password: MINDSDB_PASSWORD,
 });
 
-const allDatabases = await MindsDB.default.Databases.getAllDatabases();
-console.log(allDatabases);
+// * For getting the `mongo_atlas` database
+// const mongoAtlasDB = await MindsDB.default.Databases.getDatabase("mongo_atlas");
+// console.log(mongoAtlasDB.video_details);
+
+// * For getting all the data in the `video_details` collection
+const queryForData = `SELECT * FROM mongo_atlas.video_details`;
+const data = await MindsDB.default.SQL.runQuery(queryForData);
+
+// * For getting the comments & questions from the document we want
+const videoDetails = data.rows.find(row => {
+	if (row.title == "Docker in 100 Seconds") return row;
+});
+
+const title = videoDetails.title;
+const feedback = videoDetails.comment;
+const questions = videoDetails.question;
+
+const feedbackArray = feedback.map(comment => comment.comment);
+
+// * For getting the sentiment_classifier model
+// const sentimentClassifierModel = await MindsDB.default.Models.getModel(
+// 	"sentiment_classifier",
+// 	"mindsdb"
+// );
+
+// * For predicting a single comment
+/*
+const answer = await MindsDB.default.Models.queryModel(
+	"sentiment_classifier",
+	1,
+	"sentiment",
+	"mindsdb",
+	{ where: 'comment="I Love This!"' }
+);
+console.log("This ran!");
+console.log(answer);
+*/
+
+// * For predicting multiple comments
+// ! Not Working
+/*
+const answer = await MindsDB.default.Models.queryModel(
+	"sentiment_classifier",
+	1,
+	"sentiment",
+	"mindsdb",
+	{
+		where: `comment="I love this video" AND comment="I hate this video"`,
+	}
+);
+*/
