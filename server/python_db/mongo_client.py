cd5f1c77e9b9f227f91a2afef28f40d5eb136d58
Pranav <pranavprajapati586@gmail.com>
2023-04-28 00:31:46 +0530

    parallel processing of pymongo cursors to speed up process

--- server/python_db/mongo_client.py
+++ server/python_db/mongo_client.py
@@ -1,10 +1,9 @@
 import pickle as pkl
 from pymongo import MongoClient
-from keys import api_key, AtlasConnection_string
+from keys import api_key, AtlasConnection_string,CloudConnection_string
 from youtube_api import YoutubeAPI
-import random
-import string
 import sys
+import concurrent.futures
 
 link = sys.argv[1]
 # print(link)
@@ -24,6 +23,25 @@ for comment in comments:
     else:
         feedback_comments.append(comment)
 
+mindsdb_cloud = MongoClient(CloudConnection_string)
+mindsdb = mindsdb_cloud.mindsdb
+sentiment_classifier = mindsdb.sentiment_classifier
+pred_list = [sentiment_classifier.find({'comment':i}) for i in feedback_comments]
+pred_list1 = pred_list[:int(len(pred_list)/2)]
+pred_list2 = pred_list[int(len(pred_list)/2):]
+
+sentiment_list = []
+def process_cursor(cursor):
+    for doc in cursor:
+        sentiment_list.append(doc)
+
+# Process cursors in parallel
+with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor1, \
+    concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor2:
+    futures1 = [executor1.submit(process_cursor, cursor) for cursor in pred_list1]
+    futures2 = [executor2.submit(process_cursor,cursor)for cursor in pred_list2]
+
+    concurrent.futures.wait(futures1 + futures2)
 # Start saving comments to database in Different collections
 
 atlas_client = MongoClient(AtlasConnection_string)
@@ -31,15 +49,13 @@ atlas_client = MongoClient(AtlasConnection_string)
 #Create a Database
 comments_db = atlas_client.comments_db
 
-#Create Two collections
-
 video_details = comments_db.video_details
 
 video_details.insert_one({
     'Title':video_title,
     'Subscriber': subscriber_count,
     'Channel Name': channel_name,
-    'comment': feedback_comments,
+    'comment': sentiment_list,
     'question': interrogative_comments
 })
-print("updated database")
+print(video_title)
